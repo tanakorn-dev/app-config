ccfcb2630ebea29727b1a2e5dcccb9b1207df30d
Tanakorn Phoochaliaw <tanakorn.dev@gmail.com>
2021-03-17 00:28:17 +0700

    :tada: Init project and add first service.

---
+++ routes/app-config.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/cpfm', function(req, res, next) {
+  res.status(200).json(
+    { 
+      "success": true,
+      "app-version": "2.0.4", 
+      "is_show_third_party_login": false
+    }
+  );
+});
+
+module.exports = router;
