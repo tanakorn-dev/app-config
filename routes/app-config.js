ac9072bedca2e4fc2973e77c088e5e0814263e3f
Tanakorn Phoochaliaw <tanakorn.dev@gmail.com>
2021-05-25 14:30:16 +0700

    Update app-config.js

--- routes/app-config.js
+++ routes/app-config.js
@@ -6,8 +6,8 @@ router.get('/cpfm', function(req, res, next) {
   res.status(200).json(
     { 
       "success": true,
-      "app-version": "2.6.0", 
-      "is_show_third_party_login": true
+      "app-version": "2.7.0", 
+      "is_show_third_party_login": false
     }
   );
 });
